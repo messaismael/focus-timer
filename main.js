c8afb5da288a5a07282dac89b24acb903fc2eaa7
messaismael <messaismael01@gmail.com>
2020-02-20 16:43:32 +0100

    run pomodoro clock with electron

---
+++ main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Cree la fenetre du navigateur.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // et charger le fichier index.html de l'application.
+  win.loadFile('dist/index.html')
+
+  // Ouvre les DevTools.
+  win = null
+ }
+
+
+// Cette méthode sera appelée quant Electron aura fini
+// de s'initialiser et prêt à créer des fenêtres de navigation.
+// Certaines APIs peuvent être utilisées uniquement quand cet événement est émit.
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // Sur macOS, il est commun pour une application et leur barre de menu
+  // de rester active tant que l'utilisateur ne quitte pas explicitement avec Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // Sur macOS, il est commun de re-créer une fenêtre de l'application quand
+  // l'icône du dock est cliquée et qu'il n'y a pas d'autres fenêtres d'ouvertes.
+  if (win === null) {
+    createWindow()
+  }
+})
